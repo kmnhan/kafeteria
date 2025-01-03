ea16dd8dca140fd6cdb49bc4efe1ad3e8662e4e4
Kimoon Han <khan@kaist.ac.kr>
2025-01-03 11:23:10 +0900

    docs: add README

---
+++ README.md
@@ -0,0 +1,51 @@
+# Kafeteria
+
+Kafeteria is a Python package to asynchronously access the menu of the cafeterias at KAIST.
+
+## Installation
+
+Clone the repository and install from source:
+
+```bash
+git clone https://github.com/kmnhan/kafeteria.git
+cd kafeteria
+pip install .
+```
+
+## Usage
+
+Use the `get_menu` function to get the menu for a specific cafeteria, or the `get_menus` function to get the menus for multiple cafeterias at once.
+
+Valid cafeteria codes are:
+
+- `fclt` : 카이마루
+- `west` : 서측식당
+- `east1` : 동측식당
+- `east2` : 동측교직원식당
+- `emp` : 교수회관
+- `icc` : ICC
+
+Here's an example:
+
+```python
+import asyncio
+import datetime
+import kafeteria
+
+
+async def main():
+    # Get the menu for a specific cafeteria
+    menu = await kafeteria.get_menu("fclt")
+    print(menu)
+
+    # Get the menu for a specific date
+    menu = await kafeteria.get_menu("fclt", datetime.date(2025, 1, 3))
+    print(menu)
+
+    # Get the menus for multiple cafeterias at once
+    menus = await kafeteria.get_menus(["fclt", "east1", "east2"])
+    print(menus)
+
+
+asyncio.run(main())
+```
--- kafeteria.py
+++ kafeteria.py
@@ -1,4 +1,6 @@
-"""Get cafeteria menu from KAIST website."""
+"""Get the menu for KAIST cafeterias."""
+
+__all__ = ["get_menu", "get_menus"]
 
 import asyncio
 import datetime
@@ -56,9 +58,10 @@ class MenuParsingError(RuntimeError):
 FOOD_PATTERN = re.compile(
     r"<div class=\"item\" id=\"tab_item_1\">[\s]*?<h3>\[ ([가-힣 ]+) \].+?(\d{2}\/\d{2}\([가-힣]\))<\/h3>[\s]*?<p>([\s\S]*?)\s*?<\/p>[\s\S]*?<th scope=\"col\">조식 (.*?)<\/th>[\s]*?<th scope=\"col\">중식 (.*?)<\/th>[\s]*?<th scope=\"col\">석식 (.*?)<\/th>[\s\S]*?<!-- <ul class=\"list-1st\"> -->[\s]*([\s\S]*?)[\s]*(?:<br\/>)?[\s]*<!-- <\/ul> -->[\s\S]*?<ul class=\"list-1st\">[\s]*([\s\S]*?)[\s]*(?:<br\/>)?[\s]*<\/ul>[\s\S]*?<ul class=\"list-1st\">[\s]*([\s\S]*?)[\s]*(?:<br\/>)?[\s]*<\/ul>"  # noqa: E501
 )
+"""Regex pattern to extract menu information from the KAIST website."""
 
 
-def _make_url(cafeteria: Cafeteria, dt: datetime.datetime) -> str:
+def _make_url(cafeteria: Cafeteria, dt: datetime.date) -> str:
     return (
         "http://kaist.ac.kr/kr/html/campus/053001.html?"
         f"dvs_cd={cafeteria}&"
@@ -70,7 +73,7 @@ def _parse_group(group: str) -> str:
     return html.unescape(group).replace("<br />", "\n").replace("<br/>", "\n").strip()
 
 
-async def get_menu(cafeteria: Cafeteria, dt: datetime.datetime | None = None) -> Menu:
+async def get_menu(cafeteria: Cafeteria, dt: datetime.date | None = None) -> Menu:
     """
     Retrieve the menu for a specified cafeteria on a given date from the KAIST website.
 
@@ -79,9 +82,9 @@ async def get_menu(cafeteria: Cafeteria, dt: datetime.datetime | None = None) ->
     cafeteria : CafeteriaCode
         Code for the cafeteria. Must be one of "fclt", "west", "east1", "east2", "emp",
         "icc".
-    dt : datetime.datetime, optional
-        Datetime object representing the date to retrieve the menu for. If None, the
-        current date is used.
+    dt : datetime.date, optional
+        Date object representing the date to retrieve the menu for. If None, the current
+        date is used.
 
     Returns
     -------
@@ -94,7 +97,7 @@ async def get_menu(cafeteria: Cafeteria, dt: datetime.datetime | None = None) ->
     >>> menu = await get_menu("fclt")
     """
     if dt is None:
-        dt = datetime.datetime.now()
+        dt = datetime.date.today()
 
     if cafeteria not in get_args(Cafeteria):
         raise InvalidCafeteriaError(cafeteria)
@@ -115,7 +118,7 @@ async def get_menu(cafeteria: Cafeteria, dt: datetime.datetime | None = None) ->
 
 
 async def get_menus(
-    cafeteria_list: list[Cafeteria], dt: datetime.datetime | None = None
+    cafeteria_list: list[Cafeteria], dt: datetime.date | None = None
 ) -> list[Menu]:
     """
     Retrieve the menus for multiple cafeterias on a given date from the KAIST website.
